aa953a52b963f60868f07d0336806b5e47a953cc
RomanBuzz <buninrom@gmail.com>
2023-12-31 18:05:05 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function myFunction() {
+  let menu = document.querySelector(".menu");
+  let links = document.querySelectorAll(".menu__top");
+  if (menu.style.display === "flex") {
+    menu.style = "null";
+    for (let link of links) {
+      link.style = "null";
+    }
+  } else {
+    menu.style.display = "flex";
+    menu.style.marginTop = "110px";
+    menu.style.backgroundColor = "#d0d0d038";
+    for (let link of links) {
+      link.style.marginLeft = 0;
+    }
+  }
+};
+
+window.addEventListener('resize', function (event) {
+  let menu = document.querySelector(".menu");
+  let links = document.querySelectorAll(".menu__top");
+  menu.style = "null";
+  for (let link of links) {
+    link.style = "null";
+  }
+}, true);
+
+window.addEventListener("scroll", function (event) {
+  let upwardButton = document.querySelector(".upward__button");
+  let posTop = window.pageYOffset;
+  if (posTop < 80) {
+    upwardButton.style.display = "none";
+  } else {
+    upwardButton.style = "null";
+  }
+});
